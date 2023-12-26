659f40aef27125803a4cf0afa29b6f75bfd17615
Varga Tamás <mgodisai@gmail.com>
2023-12-26 22:54:57 +0100

    First version of WPF Button Animation has been made

---
+++ ButtonAnimationWPF/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace ButtonAnimationWPF
+{
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      private Thickness originalThickness;
+      private readonly Storyboard borderAnimationStoryboard;
+      public MainWindow()
+      {
+         InitializeComponent();
+         originalThickness = MyButton.BorderThickness;
+         borderAnimationStoryboard = new Storyboard();
+         InitializeRotateTransform();
+         InitializeBorderAnimation();
+      }
+
+      private void InitializeRotateTransform()
+      {
+         RotateTransform rotateTransform = new()
+         {
+            CenterX = MyButton.Width / 2,
+            CenterY = MyButton.Height / 2
+         };
+         MyButton.RenderTransform = rotateTransform;
+      }
+
+      private void InitializeBorderAnimation()
+      {
+         ThicknessAnimation borderAnimation = new()
+         {
+            From = originalThickness,
+            To = new Thickness(100),
+            Duration = TimeSpan.FromSeconds(10)
+         };
+
+         Storyboard.SetTarget(borderAnimation, MyButton);
+         Storyboard.SetTargetProperty(borderAnimation, new PropertyPath(Button.BorderThicknessProperty));
+         borderAnimationStoryboard.Children.Add(borderAnimation);
+      }
+
+
+      private void RotateButton()
+      {
+         if (MyButton.RenderTransform is RotateTransform rotateTransform)
+         {
+            DoubleAnimation animation = new()
+            {
+               To = rotateTransform.Angle + 15,
+               Duration = TimeSpan.FromSeconds(1)
+            };
+
+            rotateTransform.BeginAnimation(RotateTransform.AngleProperty, animation);
+         }
+      }
+
+      private void ResetRotation()
+      {
+         if (MyButton.RenderTransform is RotateTransform rotateTransform)
+         {
+            rotateTransform.BeginAnimation(RotateTransform.AngleProperty, null);
+            rotateTransform.Angle = 0;
+         }
+      }
+
+      private void MyButton_MouseEnterHandler(object sender, MouseEventArgs e)
+      {
+         if (sender is Button)
+         {
+            borderAnimationStoryboard.Begin();
+         }
+      }
+
+      private void MyButton_MouseLeaveHandler(object sender, MouseEventArgs e)
+      {
+         if (sender is Button MyButton)
+         {
+            borderAnimationStoryboard.Stop();
+            MyButton.BorderThickness = originalThickness;
+            ResetRotation();
+         }
+      }
+
+      private void MyButton_ClickHandler(object sender, RoutedEventArgs e)
+      {
+         if (sender is Button)
+         {
+            RotateButton();
+         }
+      }
+   }
+}
